ac7f24c23053e2be422e2b70bd82039d93545fb5
tennison35 <tennison35@gmail.com>
2015-11-23 21:08:05 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,163 @@
+var path = require('path');
+
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-zip');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.initConfig({
+    src: 'youtube-preview-extension',
+    dest: 'ext',
+    tmp: '.tmp',
+    builds: '../builds',
+    pkg: 'package.json',
+    manifestName: 'manifest.json',
+
+    watch: {
+      js: {
+        files: [
+          'youtube-preview-extension/**/*'
+        ],
+        tasks: [
+          'shell:reload'
+        ]
+      }
+    },
+
+    clean: {
+      post: [ '<%=tmp %>', 'js', 'css' ],
+      pre: [ '<%=dest %>' ]
+    },
+
+    jshint: {
+      files: ['Gruntfile.js', '<%=src %>/js/**/*.js', '!<%=src %>/js/jquery.js'],
+      options: {
+        curly: false,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+          // AMD
+          module: true,
+          require: true,
+          // Environments
+          console: true,
+          // General Purpose Libraries
+          $: true,
+          jQuery: true,
+          // Extension
+          chrome: true
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        compress: {
+          drop_console: true
+        }
+      },
+      content_scripts: {
+        options: {
+          compress: {
+            drop_console: true
+          },
+          // mangle: true,
+          mangle: {toplevel: true},
+          squeeze: {dead_code: false},
+          codegen: {quote_keys: true}
+        },
+        files: {
+          '<%=dest %>/youtube-preview.js': [
+            '<%=src %>/js/jquery.js',
+            '<%=src %>/js/preview.js',
+          ]
+        }
+      }
+    },
+
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            expand: true,
+            cwd: './<%=src %>/assets/images/',
+            src: ['**/*.png'],
+            dest: './<%=dest %>/assets/images/',
+            ext: '.png'
+          }
+        ]
+      }
+    },
+
+    zip: {
+      '<%=builds %>/build-<%=verison %>.zip': [ '<%=dest %>/**' ]
+    },
+
+    shell: {
+      reload: {
+        command: "chrome-cli open chrome://extensions && sleep 1 && chrome-cli reload && chrome-cli close"
+      },
+      'git-stash': {
+        command: 'git stash'
+      }
+    }
+  });
+
+  grunt.registerTask('updateRev', function (key, value) {
+    var manifestPath = grunt.config.get('src') + "/" + grunt.config.get('manifestName');
+    var manifest = grunt.file.readJSON( manifestPath );
+
+    var verison = manifest.version;
+    grunt.config('verison', verison);
+  });
+
+  grunt.registerTask('updateManifest', function (key, value) {
+    var manifestPath = grunt.config.get('src') + "/" + grunt.config.get('manifestName');
+    var manifestDest = grunt.config.get('dest') + "/" + grunt.config.get('manifestName');
+
+    if (!grunt.file.exists(manifestPath)) {
+      grunt.log.error("file " + manifestPath + " not found");
+      return true;
+    }
+    var manifest = grunt.file.readJSON(manifestPath);
+
+    grunt.log.error('version: ' + manifest.version);
+
+    // content_scripts/js
+    manifest.content_scripts[0].js = [ "youtube-preview.js" ];
+
+    grunt.file.write(manifestDest, JSON.stringify(manifest, null, 2));
+  });
+
+  grunt.registerTask('test', [ 'jshint' ]);
+  grunt.registerTask('pack', [ 'updateRev', 'shell:git-stash', 'zip' ]);
+
+  grunt.registerTask('default',
+    [
+      'clean:pre',
+      'jshint',
+      // Minify
+      'imagemin',
+      'uglify',
+      //Build
+      'updateRev',
+      'updateManifest',
+      'clean:post'
+    ]);
+};
---
+++ youtube-preview-extension/js/preview.js
@@ -0,0 +1,112 @@
+﻿(function($){
+
+"use strict";
+
+var Preview = {
+  currentPage: 0,
+  interval: 200,
+  mouseover: false,
+  initialize: function() {
+    $("a[href^='/watch']")
+      .mouseenter(this.mouseEnterEvent)
+      .mouseleave(this.mouseLeaveEvent);
+  },
+  mouseEnterEvent: function() {
+    Preview.mouseover = true;
+    var obj = $(this);
+    $.ajax({
+      dataType: "html",
+      url: obj.attr("href"),
+      success: function(html) {
+        var l2 = Preview.getStoryboardDetails(html)[2];
+        var width = l2.width, height = l2.height;
+        var el = obj.find("img").get(0);
+        var thumb = obj.find(".yt-thumb");
+        if (thumb.length) {
+          l2.width = thumb.width();
+          l2.height = thumb.height();
+          thumb.css({ width: l2.width, height: l2.height, overflow: "hidden" });
+        }
+
+        el.src = l2.url();
+        el.onload = function(e) {
+          $(el).css({
+            width: (l2.width * this.naturalWidth / width),
+            height: (l2.height * this.naturalHeight / height),
+            position: "relative"
+          });
+          setTimeout(function(){ Preview.framesPlaying(el, l2); }, Preview.interval);
+        };
+      }
+    });
+  },
+  mouseLeaveEvent: function(){
+    console.log("mouseleave");
+    Preview.mouseover = false;
+  },
+  getStoryboardDetails: function(data) {
+    var l = {}, url;
+    var getStoryboardRegExp = new RegExp("\"storyboard_spec\": ?\"(.*?)\"", "g");
+    var storyboard_spec = getStoryboardRegExp.exec(data);
+    var result = storyboard_spec[1].split("|");
+    url = result.shift();
+
+    for(var i = 1; i < result.length; i++){
+      l[i] = new Preview.Storyboard(result[i], url);
+    }
+    console.log("l", l);
+
+    return l;
+  },
+  framesPlaying: function (el, data) {
+    if (data.page() !== Preview.currentPage) {
+      Preview.currentPage = data.page();
+      el.src = data.url();
+      el.onload = function() {
+        setTimeout(function(){ Preview.framesPlaying(el, data); }, Preview.interval);
+      };
+      return false;
+    }
+
+    $(el).css({
+      left: -1 * data.width * (data.count % data.col),
+      top: -1 * data.height * (Math.floor((data.count / data.row)) % data.row)
+    });
+    data.count += 1;
+    if(Preview.mouseover) {
+      setTimeout(function(){ Preview.framesPlaying(el, data); }, Preview.interval);
+    }
+  }
+
+};
+
+Preview.Storyboard = function (str, baseUrl) {
+  var arr = str.split("#");
+
+  this.baseUrl = baseUrl;
+  this.width = arr[0];
+  this.height = arr[1];
+  this.totalFrames = arr[2];
+  this.row = arr[3];
+  this.col = arr[4];
+  this.ms = arr[5];
+  this.unit = arr[6];
+  this.sigh = arr[7];
+  this.maxPage = Math.ceil(this.totalFrames/ (this.row * this.col));
+  this.count = 0;
+
+  return this;
+};
+Preview.Storyboard.prototype.page = function() {
+  var page = Math.floor(this.count / (this.col * this.row));
+  return page % this.maxPage;
+};
+Preview.Storyboard.prototype.url = function(l, m) {
+  l = l || 2;
+  m = m || this.page();
+  return this.baseUrl.replace(/\\/g, "").replace("$L", l).replace("$N", "M" + m) + "?sigh=" + this.sigh;
+};
+
+Preview.initialize();
+
+}(jQuery));
